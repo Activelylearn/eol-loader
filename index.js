f28190be379e9fe5b8772ce9da98cecf128f4416
Stein Setvik <stein@activelylearn.com>
2017-05-05 22:32:17 -0700

    Update to replace CRLF with LF

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-/*jslint node:true */
 "use strict";
 
-var EOL = require('os').EOL;
-
 module.exports = function(content) {
-    // Replace the newlines.
-    content = content.replace(/\r?\n/g, EOL);
-    
-    // Add newline at the end of file.
-    if (!new RegExp(EOL + '$').test(content)) {
-        content += EOL;
-    }
-    
-    this.callback(null, content);
+    const eolNormalized = content.replace(/\r\n/g, "\n");
+    this.callback(null, eolNormalized);
 }
